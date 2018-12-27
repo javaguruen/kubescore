179dd8940c0b0a5770b34edbadcec11d1720c9f8
Bjørn Hamre <javaguruen@gmail.com>
2018-12-27 22:57:33 +0100

    Initial setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <!--Navbar-->
+    <mdb-navbar position="top" class="indigo" dark>
+      <!-- Navbar brand -->
+      <mdb-navbar-brand href="https://mdbootstrap.com/">
+        Navbar
+      </mdb-navbar-brand>
+      <mdb-navbar-toggler>
+        <mdb-navbar-nav>
+          <mdb-nav-item href="/" active>Home</mdb-nav-item>
+          <mdb-nav-item href="/scrambles">Scrambles</mdb-nav-item>
+          <mdb-nav-item href="#">Pricing</mdb-nav-item>
+          <!-- Dropdown -->
+          <mdb-dropdown tag="li" class="nav-item">
+            <mdb-dropdown-toggle tag="a" navLink color="indigo" slot="toggle" waves-fixed>Dropdown</mdb-dropdown-toggle>
+            <mdb-dropdown-menu>
+              <mdb-dropdown-item>Action</mdb-dropdown-item>
+              <mdb-dropdown-item>Another action</mdb-dropdown-item>
+              <mdb-dropdown-item>Something else here</mdb-dropdown-item>
+            </mdb-dropdown-menu>
+          </mdb-dropdown>
+        </mdb-navbar-nav>
+        <!-- Search form -->
+        <form class="form-inline">
+          <input class="form-control mr-auto p-2" type="text" placeholder="Search" aria-label="Search">
+        </form>
+      </mdb-navbar-toggler>
+    </mdb-navbar>
+
+
+    
+    <img src="./assets/logo-mdb-vue-small.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { mdbNavbar, mdbNavItem, mdbNavbarNav, mdbNavbarToggler, mdbContainer, mdbDropdown, mdbDropdownItem, mdbDropdownMenu, mdbDropdownToggle, mdbInput, mdbNavbarBrand, mdbIcon, mdbRow } from 'mdbvue';
+
+export default {
+  name: 'App',
+    components: {
+    mdbNavbar,
+    mdbNavItem,
+    mdbNavbarNav,
+    mdbNavbarToggler,
+    mdbContainer,
+    mdbDropdown,
+    mdbDropdownItem,
+    mdbDropdownMenu,
+    mdbDropdownToggle,
+    mdbInput,
+    mdbNavbarBrand,
+    mdbIcon,
+    mdbRow
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 100px;
+}
+</style>
---
+++ frontend/src/components/Scramble.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="scramble">
+    <h3>Scrambles</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Scramble'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  font-weight: normal;
+  padding-top: 20px;
+  padding-bottom: 30px;
+}
+p {
+  color: #969696;
+  margin-bottom: 0;
+  font-size: 14px;
+}
+</style>
