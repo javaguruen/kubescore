f9662820e6b3d9d114aaaa51f4ce3784211d386e
Bjørn Hamre <javaguruen@gmail.com>
2018-12-27 23:19:54 +0100

    Spring boot

---
+++ backend/src/main/java/no/hamre/kubescore/application/Application.java
@@ -0,0 +1,14 @@
+package no.hamre.kubescore.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
